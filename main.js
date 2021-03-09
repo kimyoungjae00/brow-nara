be74dc70ed3003ef78847b7f969ec684358e5f8c
kimyoungjae00 <kij11009@gmail.com>
2021-03-09 17:55:51 +0900

    Html : Slide

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//햄버거메뉴
 const togleBtn = document.querySelector('.navbar__togleBtn');
 const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
@@ -6,3 +7,34 @@ togleBtn.addEventListener('click', ()=>{
     menu.classList.toggle('active');
     icons.classList.toggle('active');
 });
+
+var slides = document.querySelectorAll("#slides > img");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+var current = 0;
+
+//이미지 좌우 버튼
+showSlides(current);
+prev.onclick = prevSlide;
+next.onclick = nextSlide;
+
+function showSlides(n) {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[n].style.display = "block";
+}
+
+function prevSlide() {
+  if (current > 0) current -= 1;
+  else
+    current = slides.length - 1;
+    showSlides(current);
+}
+
+function nextSlide() {
+  if (current < slides.length - 1) current += 1;
+  else
+    current = 0;
+    showSlides(current);  
+}
